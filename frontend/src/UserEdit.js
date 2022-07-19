3d4b0fbe35b474043e49c13a6d902c4cfeea9870
Ruan Macambira do Nascimento <ruanmacambira@gmail.com>
2022-07-19 16:39:30 -0300

    Editar formulário de Usuário para responder a erro de validação

--- frontend/src/UserEdit.js
+++ frontend/src/UserEdit.js
@@ -7,6 +7,7 @@ import AppNavBar from './AppNavBar'
 function UserEdit(props) {
     const emptyItem = { nome: "", nomeMae: "" }
     const [item, setItem] = useState(emptyItem)
+    const [errors, setErrors] = useState({})
     const navigate = useNavigate()
     const params = useParams()
 
@@ -22,17 +23,21 @@ function UserEdit(props) {
     })
 
     const handleChange = event => {
-        console.log(event.target.name, event.target.value)
         const {target: {value, name}} = event
+
         const newItem = {...item}
         newItem[name] = value
         setItem(newItem)
+
+        const newErrors = {...errors}
+        newErrors[name] = null
+        setErrors(newErrors)
     }
 
     const handleSubmit = async event => {
         event.preventDefault()
 
-        await fetch(`/usuarios/${item.id ?? ''}`, {
+        const response = await fetch(`/usuarios/${item.id ?? ''}`, {
             method: (item.id) ? 'PUT' : 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -40,7 +45,17 @@ function UserEdit(props) {
             },
             body: JSON.stringify(item)
         })
-        navigate('/usuarios')
+
+        switch(response.status) {
+        case 201:
+            navigate('/usuarios')
+            break
+        case 400:
+            const body = await response.json()
+            setItem(body.usuario)
+            setErrors(body.errors)
+            break
+        }
     }
 
     const title = <h2>{item.id ? 'Alterar Usuário' : 'Novo Usuário'}</h2>
@@ -50,16 +65,30 @@ function UserEdit(props) {
             <AppNavBar/>
             <Container>
                 {title}
-                <Form onSubmit={handleSubmit}>
+                <Form onSubmit={handleSubmit} novalidate={true}>
                     <FormGroup>
                         <Label for="nome">Nome</Label>
-                        <Input type="text" name="nome" id="nome" value={item.nome || ''}
-                               onChange={handleChange} autoComplete="name"/>
+                        <Input type="text" name="nome" id="nome" invalid={errors.nome} value={item.nome ?? ''} required={true}
+                               onChange={handleChange}/>
+                        <div className="invalid-feedback">{errors.nome}</div>
                     </FormGroup>
                     <FormGroup>
                         <Label for="nomeMae">Nome da Mãe</Label>
-                        <Input type="text" name="nomeMae" id="nomeMae" value={item.nomeMae || ''}
-                               onChange={handleChange} autoComplete="name"/>
+                        <Input type="text" name="nomeMae" id="nomeMae" invalid={errors.nomeMae} value={item.nomeMae ?? ''} required={true}
+                               onChange={handleChange}/>
+                        <div className="invalid-feedback">{errors.nomeMae}</div>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="cpf">CPF</Label>
+                        <Input type="text" pattern="[0-9]" name="cpf" id="cpf" invalid={errors.cpf} value={item.cpf ?? ''} required={true}
+                               onChange={handleChange}/>
+                        <div className="invalid-feedback">{errors.cpf}</div>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="rg">RG</Label>
+                        <Input type="text" pattern="[0-9]" name="rg" id="rg" invalid={errors.rg} value={item.rg ?? ''} required={true}
+                               onChange={handleChange}/>
+                        <div className="invalid-feedback">{errors.rg}</div>
                     </FormGroup>
                     <FormGroup>
                         <Button color="primary" type="submit">Confirmar</Button>
